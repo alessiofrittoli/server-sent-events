08469a672bfd108c0141b525915cc5f19b785a02
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-22 20:39:45 +0100

    feature: improved build ext file

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig( {
-	entry		: [ "./src/**/*.ts" ],
+	entry		: [ 'src/**/*.ts' ],
 	format		: [ 'cjs', 'esm' ],
 	dts			: true,
 	splitting	: false,
@@ -9,4 +9,16 @@ export default defineConfig( {
 	skipNodeModulesBundle: true,
 	clean		: true,
 	treeshake	: true,
+	minify		: true,
+	outExtension( ctx ) {
+		if ( ctx.format === 'esm' ) {
+			return {
+				dts	: '.d.ts',
+				js	: '.mjs',
+			}
+		}
+		return {
+			js: '.cjs'
+		}
+	},
 } )
